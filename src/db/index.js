d9b09c6fadedd58439453a7f5135e4d8e097a39d
anshumancodes <anshumanprof01@gmail.com>
2024-06-12 09:54:17 +0530

    added db connection file

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () =>{
+    try {
+     
+    const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+    console.log(`\n mongodb sucessfully connected! DB Host = ${connectionInstance.connection.host}`)
+
+        
+    } catch (error) {
+        console.error("MONGODB CONNECTION ERROR! -> ",error);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
