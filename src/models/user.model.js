b490939c6a24fe315482f4fc663c071e92817b24
anshumancodes <anshumanprof01@gmail.com>
2024-06-17 23:15:37 +0530

    modifed user.model to add functions to generate jwt access and refresh tokens and enable password encryption and comparision

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose ,{Schema} from "mongoose";
+import  Jwt  from "jsonwebtoken";
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
     username:{
@@ -51,5 +53,39 @@ const userSchema = new Schema({
     }
 
 },{timestamps:true});
+
+// password encryption before saving the password
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next(); // prevents hashing if password isnt modified !
+    
+    // if password is modfied saves new password with encryption
+    this.password = bcrypt.hash(this.password,10);
+    next()
+})
+
+
+// compares password
+userSchema.methods.isPasswordCorrect=async function(password){
+   return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken=function(){
+   return Jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+    },
+    process.env.ACESS_SECRET_KEY,{expiresIn:process.env.ACESS_TOKEN_EXPIRY}
+    )
+}
+userSchema.methods.generateRefreshToken=function(){
+    return Jwt.sign({
+        _id:this._id,
+        
+    },
+    process.env.REFRSH_TOKEN_KEY,{expiresIn:process.env.REFRSH_TOKEN_EXPIRY}
+    )
+}
 
 export const User = mongoose.model("User",userSchema)
