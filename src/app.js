8a50d5e9ebb50331220691da2d372ddc4971c532
anshumancodes <anshumanprof01@gmail.com>
2024-06-22 00:11:51 +0530

    add app.js

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from  "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+
+const app = express();
+app.use(cors());
+app.use(express.json({limit:"12kb"}));
+app.use(express.urlencoded({extended:true,limit:"12kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// routes
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v0/user",userRouter);
+
+
+
+
+export default app;
