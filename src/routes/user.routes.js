dd0366c429dfe27cc06b2bf5403b421fba7650b3
anshumancodes <anshumanprof01@gmail.com>
2024-06-23 23:31:15 +0530

    modified multer middleware is now on user route to get files

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { resigterUser } from "../controllers/user.controller.js";
+import {upload} from"../middlewares/multer.middleware.js"
 const router=Router();
 
-router.route("/register").post(resigterUser)
+router.route("/register").post(
+upload.fields([
+    {name:"avatar",maxCount:1},
+    {name:"coverImage",maxCount:1}]),
+resigterUser)
 
 export default router;
