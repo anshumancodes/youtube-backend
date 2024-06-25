51cb7a299d5b137ffddecc4f1bf6103f606c8f97
anshumancodes <anshumanprof01@gmail.com>
2024-06-25 21:53:52 +0530

    modifed auth.middleware , defined verifyjwt method further to check and remove token if user got a token to perform auth ops like logout

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,11 +1,28 @@
-import { ApiError } from "../utils/ApiError";
-import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
-
+import {User} from "../models/user.model.js"
 const verifyJwt=asyncHandler(async(req,res,next)=>{
-    req.cookies?.accessToken || req.header ("Authorization")?.replace("Bearer ", "");
+  try {
+     const token= req.cookies?.accessToken || req.header ("Authorization")?.replace("Bearer ", "");
+  
+      if(!token){
+          throw new ApiError(401,"Unauthorized Request")
+      }
+     const verifiedToken=jwt.verify(token,process.env.ACESS_SECRET_KEY);
+     const user= await User.findById(verifiedToken._id).select("-password -refeshToken");
+  
+     if(!user){
+      throw new ApiError(401,"Invalid access Token")
+     }
+     req.user=user;
 
-    if(!token){
-        throw new ApiError(401,"Unauthorized Request")
-    }
-})
+     next()
+  } catch (error) {
+    throw new ApiError(401,error? error.message : "Invalid access token" )
+  }
+
+}) ;
+
+
+export {verifyJwt}
