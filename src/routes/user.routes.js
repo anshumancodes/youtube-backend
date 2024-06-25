df75e9c574bcdc7dd1156eef384e4e1c55259d0b
anshumancodes <anshumanprof01@gmail.com>
2024-06-26 00:29:22 +0530

    add paths and endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
-import { resigterUser } from "../controllers/user.controller.js";
+import { resigterUser  , loginUser,logOutUser,reassignAcessToken } from "../controllers/user.controller.js";
 import {upload} from"../middlewares/multer.middleware.js"
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router=Router();
 
 router.route("/register").post(
 upload.fields([
     {name:"avatar",maxCount:1},
     {name:"coverImage",maxCount:1}]),
-resigterUser)
+resigterUser);
+
+router.route("/login").post(loginUser);
+
+
+// accessible on login;
+
+router.route("/logout").post(verifyJwt,logOutUser);
+router.route("/refreshtoken").post(reassignAcessToken)
 
 export default router;
