44d3fcb6df885e3d2bf3d140185ea25fa8eb9a10
anshumancodes <anshumanprof01@gmail.com>
2024-07-01 15:57:42 +0530

    add await in password encryption method

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -59,7 +59,7 @@ userSchema.pre("save",async function(next){
     if(!this.isModified("password")) return next(); // prevents hashing if password isnt modified !
     
     // if password is modfied saves new password with encryption
-    this.password = bcrypt.hash(this.password,10);
+    this.password = await bcrypt.hash(this.password,10);
     next()
 })
 
@@ -89,3 +89,4 @@ userSchema.methods.generateRefreshToken=function(){
 }
 
 export const User = mongoose.model("User",userSchema)
+
