221fadff19ac7db91c8e57257b9701876bda2732
anshumancodes <anshumanprof01@gmail.com>
2024-07-02 23:39:24 +0530

    add new user endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { resigterUser  , loginUser,logOutUser,reassignAcessToken } from "../controllers/user.controller.js";
+import { resigterUser  , loginUser,logOutUser,reassignAcessToken, changePassword, getCurrentUser, HandleForgotPassword, updateUserAvatar, updateCoverImage, getUserChannel } from "../controllers/user.controller.js";
 import {upload} from"../middlewares/multer.middleware.js"
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router=Router();
@@ -16,6 +16,17 @@ router.route("/login").post(loginUser);
 // accessible on login;
 
 router.route("/logout").post(verifyJwt,logOutUser);
-router.route("/refreshtoken").post(reassignAcessToken)
+router.route("/refreshtoken").post(reassignAcessToken);
+router.route("/changepassword").post(verifyJwt,changePassword);
+router.route("/getuser").get(verifyJwt,getCurrentUser);
+router.route("/updateavatar").post(verifyJwt,updateUserAvatar);
+router.route("/updatecoverimage").post(verifyJwt,updateCoverImage);
+router.route("/getchannel").get(getUserChannel);
+
+// can acess with or without a token or login
+
+router.route("passwordreset").post(HandleForgotPassword);
+
+
 
 export default router;
