3ca134f3545ed024fd37edde8ffcf114b98730aa
anshumancodes <anshumanprof01@gmail.com>
2024-08-05 02:57:30 +0530

    add new update and delete routes

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,6 +1,6 @@
-import { verifyJwt } from "../middlewares/auth.middleware";
-import { upload } from "../middlewares/multer.middleware";
-import { uploadVideoOnchannel } from "../controllers/video.controller";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { uploadVideoOnchannel ,UpdateVideoDetails,getVideoById, deleteVideo} from "../controllers/video.controller.js";
 
 
 
@@ -9,9 +9,13 @@ import { Router } from "express";
 const router=Router();
 
 
-router.route("/upload",verifyJwt,upload.fields([
+router.route("/upload").post(verifyJwt,upload.fields([
     {name:"thumbnail",maxCount:1},
     {name:"videoFile",maxCount:1}]),uploadVideoOnchannel);
+
+router.route("/play/:videoId").get(getVideoById); // videos can be played without tokens 
+router.route("/update/:videoId").put(verifyJwt,upload.fields([{name:"thumbnail",maxCount:1},]),UpdateVideoDetails); // can be done by owner only
+router.route("/delete/:videoId").delete(verifyJwt,deleteVideo)
 
 
 export default router;
