81b2842fa5444086e1148d697e77218da9e34e55
anshumancodes <anshumanprof01@gmail.com>
2024-08-11 03:27:44 +0530

    add refractor and model export

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -3,15 +3,18 @@ import mongoose ,{Schema} from "mongoose";
 const subscriptionSchema= new Schema({
 
     subscriber:{
-        type:Schema.Types.ObjectId,
+        type:Schema.Types.ObjectId,     // one who is subscribing  -subscriber id 
         ref:"User"
     },
     channel:
         {
-            type:Schema.Types.ObjectId,
+            type:Schema.Types.ObjectId,  // one to whom 'subscriber' is subscribing - channel id 
             ref:"User"
         }
     
 
 
 },{timestamps:true})
+
+
+export const Subscription=mongoose.model("Subscription",subscriptionSchema);
