e9701a17e87a26168598f83f7770a9d1c19961f7
anshumancodes <anshumanprof01@gmail.com>
2024-08-13 02:59:16 +0530

    add compose posts update delete routes for community posts

--- src/routes/community.routes.js
+++ src/routes/community.routes.js
@@ -1,7 +1,20 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
+import{getPosts,deletePost,updatePost,composePost} from "../controllers/communityPost.controller.js"
 
 
-const router=Router;
+const router=Router();
 
-router.route("/post").post(verifyJwt,)
+router.route("/compose").post(verifyJwt, composePost);
+
+// Route to get all posts for the authenticated user
+router.route("/posts").get(verifyJwt, getPosts);
+
+// Route to update a specific post
+router.route("/update/:postId").put(verifyJwt, updatePost);
+
+// Route to delete
+router.route("/delete/:postId").delete(verifyJwt, deletePost);
+
+
+export default router;
