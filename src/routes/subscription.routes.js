eab2127a181b01f8acc5a4f030b9a1f47c66a755
anshumancodes <anshumanprof01@gmail.com>
2024-11-07 21:41:56 +0530

    modify subscription routes

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import {getUserChannelSubscribers,toggleSubcription} from "../controllers/subscription.controller.js"
+import {getUserChannelSubscribers,toggleSubscription} from "../controllers/subscription.controller.js"
 
 const router=Router();
 
-router.route("/subscribe/:channelId").post(verifyJwt,toggleSubcription);
+router.route("/subscribe/:channelId").post(verifyJwt,toggleSubscription);
 router.route("/subscribers/:channelId").get(getUserChannelSubscribers);
 
 export default router;
